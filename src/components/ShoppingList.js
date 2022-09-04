9ed71a95fd6b4939236862bd2a3aceee9fad3f50
Paul Jung <pbnjcub@gmail.com>
2022-09-04 17:16:58 -0400

    commit tree 2f5fe54ae56d55fc4da731b731bbd19ee77c45b5

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
+import itemData from "../data/items";
+
+// In the ShoppingList component, there is a <select> element that will allow us to filter the list of items by category.
+
+// Use the useState hook to create a state variable called selectedCategory for keeping track of the selected value from this <select> element. When the value of the <select> element is changed, update state.
+
+// You should also use the selectedCategory variable to determine which items to display in the shopping list. You'll need some way of filtering the array of items based on the selectedCategory.
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState('All')
+  const [itemsList, setItems] = useState(itemData)
+  
+  const itemsToDisplay = itemsList.filter((item) => {
+    if (selectedCategory === 'All') {
+      return true
+    } else {
+      return item.category === selectedCategory
+    }
+  })
+
+  function filterCategory(e) {
+    e.preventDefault()
+    setSelectedCategory(e.target.value)
+  }
+
   return (
-    <div className="ShoppingList">
+    <div className="ShoppingList" onChange={filterCategory}>
       <div className="Filter">
         <select name="filter">
           <option value="All">Filter by category</option>
@@ -13,7 +36,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
