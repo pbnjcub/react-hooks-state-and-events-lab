e64674ec24f792172349634194f81dbc21751a76
Paul Jung <pbnjcub@gmail.com>
2022-09-04 17:17:16 -0400

    lab done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [itemIn, setItemIn] = useState("")
+
+  function addItem (e) {
+    e.preventDefault()
+    setItemIn("in-cart")
+
+  }
+
   return (
-    <li className="">
+    <li className={itemIn}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addItem}>Add to Cart</button>
     </li>
   );
 }
